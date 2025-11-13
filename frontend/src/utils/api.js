bb6a35d9b134706cd4f024a2919e262808c49e18
hillarymukuka <hillarymukuka@gmail.com>
2025-11-13 18:43:52 +0200

    Fix double slash in API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
+const apiURL = baseURL.endsWith('/') ? baseURL + 'api' : baseURL + '/api'
+
 const api = axios.create({
-  baseURL: (import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000') + '/api'
+  baseURL: apiURL
 })
 
 api.interceptors.request.use((config) => {
